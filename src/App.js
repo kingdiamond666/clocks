e77b6e97e73f7e38747a071fdf342e7a291c4003
Christian <christiancollins743@gmail.com>
2019-02-28 18:04:51 -0800

    preparing for deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import Background from './tevin-trinh-392388.jpg';
 import './App.css';
 import Home from './Home';
